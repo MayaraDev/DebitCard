e1e131cae70e97fc992e47bd3583dfe5d31764f6
Mayara Matos <mayaramatosdev@gmail.com>
2023-02-23 13:15:09 -0300

    create debitcard controller

---
+++ DebitCard/Controllers/DebitCardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebitCard.Controllers
+{
+    public class DebitCardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
